4cc56f3453b1019893bda1aa7d9d35364d44ec4d
LivMenzies <liv.menzies@outlook.com>
2025-08-04 14:11:20 -0400

    player and camera scripts
    made playercontroller and followplayer scripts, only followplayer is done. also added player w box collider and rigid body

---
+++ CosmicConquest-C4GD/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+    }
+}
