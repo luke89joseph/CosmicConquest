aa89b0914fc84229b35f97b92e5364d22809504f
luke89joseph <luke89joseph@gmail.com>
2025-08-05 13:56:57 -0400

    Revert "Update healthbar to move left and right"
    This reverts commit 6e20a296a89c2049b46177537a94f7e5f67e5eba.

--- CosmicConquest-C4GD/Assets/Scripts/health.cs
+++ CosmicConquest-C4GD/Assets/Scripts/health.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class health : MonoBehaviour
+public class Health : MonoBehaviour
 {
+    public float maxHP = 10f;
+    public float currHP = 0f;
+    public bool player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currHP = maxHP;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TakeDamage(float amt)
     {
-        
+        currHP -= amt;
+
+        if (currHP <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
