38682fea6aa8ac57b51ec165de9167302cac0fd2
fhyer <finn.hyer@gmail.com>
2025-08-04 20:55:46 -0400

    made backround loop

--- CosmicConquest-C4GD/Assets/Scripts/PlayerController.cs
+++ CosmicConquest-C4GD/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,9 @@ public class PlayerController : MonoBehaviour
     private float horInput =10f;
     public float moveSpeed = 8f;
     public float jumpSpeed;
+    public Transform groundCheckPoint;
+    public LayerMask groundLayer;
+    float groundCheckRadius = 0.2f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,11 +25,16 @@ public class PlayerController : MonoBehaviour
         horInput = Input.GetAxisRaw("Horizontal");
         float nextVelocityX = horInput * moveSpeed;
         float nextVelocityY = rb.velocity.y;
-        if (Input.GetKeyDown(KeyCode.Space))
+        bool isGrounded = checkGround();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             nextVelocityY = jumpSpeed;
             
         }
         rb.velocity = new Vector2(nextVelocityX, nextVelocityY);
+    }
+    bool checkGround()
+    {
+        return Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
     }
 }
